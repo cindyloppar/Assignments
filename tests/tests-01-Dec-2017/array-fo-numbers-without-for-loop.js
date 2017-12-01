6e4130bd30212ad7d38d5d5ee3c8de676916c001
cindyloppar <cindyloppar@gmail.com>
2017-12-01 12:04:16 +0200

    added filter

--- tests/tests-01-Dec-2017/array-fo-numbers-without-for-loop.js
+++ tests/tests-01-Dec-2017/array-fo-numbers-without-for-loop.js
@@ -1,11 +1,23 @@
 function arrayOfNumbers(arr, num, string) {
-   var newArr = []
-  
+    var newArr = []
+
     if (string === "LessThan" && arr < num) {
-                newArr.push(arr);
-                
-            }
-            return newArr;
+
+        var filtered = arr.filter(function (numbers) {
+            return numbers < num;
+
+        })
+    }
+    else if (string === "GreaterOrEqual" && arr < num) {
+
+        var filtered = arr.filter(function (numbers) {
+            return numbers >= num;
+
+        })
+    }
+
+
+    return filtered;
 }
 
 console.log(arrayOfNumbers([1, 2, 3, 4, 5, 6, 7], 3, "GreaterOrEqual"));
