a6cdca6b888c0baadbd5feec72fad892729a98d5
cindyloppar <cindyloppar@gmail.com>
2017-12-15 13:26:19 +0200

    I renamed and added a function for errors

--- twitter-app/index.js
+++ twitter-app/index.js
@@ -1,37 +1,43 @@
+
+
 var request = require('request');
 var express = require("express");
 var myapp = express();
-const url = 'https://api.twitter.com/1.1/statuses/update.json';
-var oauth = {
-    consumer_key: "IPuxZ08g4OGleP6jjlR9pIgg7",
-    consumer_secret: "W6mbeS8gHWpN9yvrNsxsRQkgEbFFCc8z7Zme9TSfDqShGHl52J",
-    token: "4900007938-gYKUiw9WgEyhwKVYgMp3M8WcjNiJcU5p51KFEO1",
-    token_secret: "ulYnmItVRdbakx4YYffxLBsxaWKKdzkeJC1n2JdghRoy5"
-};
-    access_point = {
+var qs = require('querystring')
+//  var req_data = qs.parse(body)
+
+myapp.get("/tweet", function (req, res) {
+
+    const url = 'https://api.twitter.com/1.1/statuses/update.json';
+
+    var oauth = {
+        consumer_key: "IPuxZ08g4OGleP6jjlR9pIgg7",
+        consumer_secret: "W6mbeS8gHWpN9yvrNsxsRQkgEbFFCc8z7Zme9TSfDqShGHl52J",
+        token: "4900007938-gYKUiw9WgEyhwKVYgMp3M8WcjNiJcU5p51KFEO1",
+        token_secret: "ulYnmItVRdbakx4YYffxLBsxaWKKdzkeJC1n2JdghRoy5"
+    };
+    var statusUpdateOptions = {
         url: url,
         oauth: oauth,
         qs: {
-            status: "It's working"
+            status: req.query.status
         }
     };
 
-;
-request.post(access_point, function (err, res, body) {
-    console.log("http res code", res.statusCode)
-    console.log("http res body", res.body)
-    if (err) {
-        console.log(err);
+    request.post(statusUpdateOptions, function (err, postResponse, body) {
+        console.log('error', err);
+        if (err || postResponse.statusCode.status >= 400) {
+            res.status(500).end();
 
+      
     }
-    myapp.get("/", function (req, res) {
-        res.send("hello world");
+    
     });
-
-    myapp.listen(3000, () => console.log("working"));
-
+});
 
 
 
-})
+myapp.get('/tweet', (req, res) => res.send('<h1>Hello World!</h1>'));
+
+myapp.listen(3000, () => console.log('The server is listening on port 3000!'))
 
--- twitter-app/server.js
+++ twitter-app/server.js
@@ -1,7 +1,27 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+ href = "https://api.twitter.com/oauth/access_token" 
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// 
+//     , oauth =
+//         {
+//             callback: 'http://mysite.com/callback/'
+//             , consumer_key: " IPuxZ08g4OGleP6jjlR9pIgg7"
+//             , consumer_secret: "W6mbeS8gHWpN9yvrNsxsRQkgEbFFCc8z7Zme9TSfDqShGHl52J"
+//         }
+//     , url = 'https://api.twitter.com/oauth/access_token'
+//     ;
+// request.post({ url: url, oauth: oauth }, function (e, r, body) {
+//     // Ideally, you would take the body in the response
+//     // and construct a URL that a user clicks on (like a sign in button).
+//     // The verifier is only available in the response after a user has
+//     // verified with twitter that they are authorizing your app.
+
+
+//    
+//     var uri = 'https://api.twitter.com/oauth/access_token'
+//         + '?' + qs.stringify({ oauth_token: req_data.oauth_token })
