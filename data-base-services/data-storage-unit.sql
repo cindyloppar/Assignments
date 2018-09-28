0243e6f870820b2f3491e81e224f7e17db191355
cindyloppar <cindyloppar@gmail.com>
2018-09-28 16:42:59 +0200

    inserted data in the tables.

--- data-base-services/data-storage-unit.sql
+++ data-base-services/data-storage-unit.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS business(
     contact_email VARCHAR(100)
 ); CREATE TABLE IF NOT EXISTS location(
     id serial PRIMARY KEY,
-    Address VARCHAR (100),
+    address VARCHAR (100),
     business_id INT REFERENCES business(id)
 ); CREATE TABLE IF NOT EXISTS block(
     id serial PRIMARY KEY,
@@ -24,3 +24,11 @@ CREATE TABLE IF NOT EXISTS business(
     unit_width VARCHAR (100),
     unit_height VARCHAR (100)
 );
+
+INSERT INTO business(business_name, contact_name, contact_number, contact_email) VALUES ('Lauper', 'Cindy', '0735698213', 'prettydressers@gmail.com');
+INSERT INTO location(address) VALUES ('125 Tshepo Section');
+INSERT INTO unit_types(unit_type_name, unit_width) VALUES ('garage', '3 meters');
+INSERT INTO block(block_name) VALUES ('serial');
+
+-- SELECT * FROM block;
+-- SELECT * FROM unit INNER JOIN unit_types ON unit.unit_type_id = unit_types.id WHERE unit_width = '3 meters' 
